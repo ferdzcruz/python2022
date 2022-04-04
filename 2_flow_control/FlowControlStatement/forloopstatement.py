d8319ed257c39ad180953f368ae7a3ec3d5bff06
“ferdz31ph” <ferdz31ph@gmail.com>
2022-04-04 17:13:23 +0800

    day3

---
+++ 2_flow_control/FlowControlStatement/forloopstatement.py
@@ -0,0 +1,20 @@
+# print ("My name is")
+# for i in range(5):
+#     print(f"Ferdie Cruz time ({i})" )
+
+
+#Program that multiply the number give by user 10x
+# 
+
+# num = input()
+# mynum = int(num)
+# for i in range (11):
+#     print(f"{mynum}x{i} = {mynum*i}")
+
+
+
+mynum = input()
+for a in range(10):
+    usernum = int(mynum)
+    print(f"{mynum} + {a} = {usernum + a} : {mynum} x {a} = {usernum * a}")
+   
--- module/addnum.py
+++ module/addnum.py
@@ -1,3 +1,4 @@
 from mainfile import add
 print(add(10,10))
 print(add(20,20))
+
